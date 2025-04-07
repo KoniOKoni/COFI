588539b2f35c83b6c1a17b3e21e9e34a3befce43
SHKim <shyeonkim0407@gmail.com>
2025-04-07 17:06:28 +0900

    First commit

---
+++ boltzmann.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#define OmegaDM 0.265
+#define OmegaB 0.04939
+#define OmegaDE 0.679
+#define aeq 3e-4
+
+using namespace std;
+
+/*Matter = decaying dark matter + baryon (No separated CDM components as assumed in the note)*/
+double OmegaDDM
+
+int main()
+{
+
+}
+
+
