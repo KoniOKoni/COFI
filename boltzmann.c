007f5c50d695c250c94cd48ecd8cef0e5ea2b483
KoniOKoni <shyeonkim0407@gmail.com>
2025-04-11 17:37:18 +0900

    Solver is changed to BDF2

--- boltzmann.c
+++ boltzmann.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /************************/
 /*Raw output is loged quantity.*/
@@ -9,19 +10,10 @@
 
 #define AEQ 2.93e-4 /*Scale factor at matter-radiation equality*/
 #define PI 3.141592
-#define Gammad -3.5
+#define Gammad -5.0
 #define DNeff 0.6384 /*Delta N_eff at a_tr*/
 #define ainit 1e-10
 #define atr 5.10479e-5
-
-/*Conversion factors*/
-#define KG_to_GeV 5.62e26
-#define M_to_GeV 5.07e15
-#define S_to_GeV 1.519e24
-#define M_to_Mpc 3.09e22
-#define Gyr_to_Mpc 306.6
-#define C 299792458
-#define GeV_to_Mpc 1.563e28
 
 #define Omega_ddm 0.2523
 #define Omega_b 0.0443678
@@ -34,6 +26,10 @@
 #define RHOC 3.67e-11 /*eV^4*/
 #define G 6.70746e-57 /*eV^-2*/
 
+#define TOL 1e-10
+#define MAX_ITER 100
+
+double coeff[3] = {3.0/2.0, -2.0, 1.0/2.0};
 
 /*Conformal Hubble constant in eV^-1*/
 double H(double x, double rho_ddm, double rho_CFT)
@@ -48,59 +44,63 @@ double H(double x, double rho_ddm, double rho_CFT)
     return exp(0.5*log_H2);
 }
 
-/*Evolution equation of log(rho)*/
-double DDMEQ(double x, double rho_ddm, double rho_CFT, double Gamma0)
+/*Jacobian*/
+void jacobian(double x, double *rho, double Gamma0 ,double jac[][2], double a, double h)
 {
-    return -3.0 - (Gamma0/H(x, exp(rho_ddm), exp(rho_CFT))) * exp((Gammad + 1)*x);
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++){
+            jac[i][j] = 0;
+        }
+    }
+    double Gamma = Gamma0*exp((Gammad + 1)*x);
+    double Hub = H(x, exp(rho[0]), exp(rho[1]));
+    double k = Gamma/Hub;
+
+    jac[0][0] = (a - exp(rho[1] - rho[0])*h*k)/(pow(a,2) - a*exp(rho[1]-rho[0])*h*k);
+    jac[0][1] = 0.0;
+    jac[1][0] = -1*exp(rho[1]-rho[0])*h*k/(pow(a,2) - a*exp(rho[1]-rho[0])*h*k);
+    jac[1][1] = a/(pow(a,2) - a*exp(rho[1]-rho[0])*h*k);
 }
 
-double CFTEQ(double x, double rho_ddm, double rho_CFT, double Gamma0)
+/*Evolution equation of rho*/
+void deriv(double x, double *rho, double *drho, double Gamma0)
 {
-    return -4.0 + (Gamma0/H(x, exp(rho_ddm), exp(rho_CFT)) * exp((Gammad + 1)*x))*exp(rho_ddm - rho_CFT);
+    double h = H(x, exp(rho[0]), exp(rho[1]));
+    drho[0] = -3 - (Gamma0/h)*exp((Gammad+1)*x);
+    drho[1] = -4 + (Gamma0/h)*exp((Gammad+1)*x)*exp(rho[1] - rho[0]);
+}
+
+void bdf2(double x_next, double h, double Gamma0 ,double y_prev[], double y_prev2[], double y_next[], double jac[][2])
+{
+    double y_guess[2];
+    memcpy(y_guess, y_prev, sizeof(double)*2);
+
+    for (int iter = 0; iter < MAX_ITER; iter++){
+        double F[2];
+        double df[2];
+        double delta[2] = {0.0,0.0};
+        deriv(x_next, y_guess, df, Gamma0);
+        jacobian(x_next, y_guess, Gamma0, jac, coeff[0], h);
+
+        for (int i = 0; i < 2; i++){
+            F[i] = coeff[0]*y_guess[i] + coeff[1]*y_prev[i] + coeff[2]*y_prev2[i] - h*df[i];
+            for (int j = 0; j < 2; j++){
+                delta[i] += -F[j]*jac[i][j];
+            }
+            y_guess[i] += delta[i];
+        }
+
+        double max_delta = 0.0;
+        for (int i = 0; i < 2; i++){
+            if (fabs(F[i]) > max_delta) max_delta = fabs(F[i]); 
+        }
+        if (max_delta < TOL) break;
+    }
+    memcpy(y_next, y_guess, sizeof(double) * 2);
 }
 /************************************************************************************* */
 
-void Boltzmann(double x0, double ddm0, double CFT0, double h, int step, double Gamma0, double *DDM, double *CFT, char *s)
-{
-    FILE *output;
-    output = fopen(s, "w");
-    double x = x0;
-    double rho_ddm = log(ddm0);
-    double rho_CFT = log(CFT0);
-
-    DDM[0] = exp(rho_ddm);
-    CFT[0] = exp(rho_CFT);
-
-    fprintf(output, "# x\t\tlog(rho_ddm)\t\tlog(rho_CFT)\n");
-    fprintf(output, "%e\t%e\t%e\t%e\t%e\n", x, rho_ddm, rho_CFT, H(x, exp(rho_ddm), exp(rho_CFT)), Gamma0*exp(Gammad*x));
-
-    for (int i = 0; i < step; i++){
-        double k1_1 = DDMEQ(x, rho_ddm, rho_CFT, Gamma0);
-        double k1_2 = CFTEQ(x, rho_ddm, rho_CFT, Gamma0);
-
-        double k2_1 = DDMEQ(x+h/2.0, rho_ddm+h*k1_1/2.0, rho_CFT+h*k1_2/2.0, Gamma0);
-        double k2_2 = CFTEQ(x+h/2.0, rho_ddm+h*k1_1/2.0, rho_CFT+h*k1_2/2.0, Gamma0);
-
-        double k3_1 = DDMEQ(x+h/2.0, rho_ddm+h*k2_1/2.0, rho_CFT+h*k2_2/2.0, Gamma0);
-        double k3_2 = CFTEQ(x+h/2.0, rho_ddm+h*k2_1/2.0, rho_CFT+h*k2_2/2.0, Gamma0);
-
-        double k4_1 = DDMEQ(x+h, rho_ddm+h*k3_1, rho_CFT+h*k3_2, Gamma0);
-        double k4_2 = CFTEQ(x+h, rho_ddm+h*k3_1, rho_CFT+h*k3_2, Gamma0);
-
-        rho_ddm += h*(k1_1 + 2*k2_1 + 2*k3_1 + k4_1)/6.0;
-        rho_CFT += h*(k1_2 + 2*k2_2 + 2*k3_2 + k4_2)/6.0;
-        x += h;
-
-        DDM[i+1] = exp(rho_ddm);
-        CFT[i+1] = exp(rho_CFT);
-
-        fprintf(output, "%e\t%e\t%e\t%e\t%e\n", x, rho_ddm, rho_CFT, H(x, exp(rho_ddm), exp(rho_CFT)), Gamma0*exp(Gammad*x));
-    }
-    fclose(output);
-    printf("Result is saved in %s.\n", s);
-}
-
-/*Interpolation after atr*/
+/*Interpolation*/
 /*rho(a) = rho(atr)*a^-3 (a > atr)*/
 double interpolation(double *rho, double a)
 {
@@ -116,38 +116,62 @@ double interpolation(double *rho, double a)
 int main()
 {
     /*General parameters*/
-    double h = -1e-5;
+    double h = 1e-5;
     double Gamma0 = 1e3; /*Gamma(a) ~ Gamma_0 * a^(Gamma_d) in Gyr^-1*/
     int N = (int)((log(atr) - log(ainit))/fabs(h)); /*The number of steps*/
 
     /*Convert to eV units*/
     Gamma0 *= 2.085e-50; /*eV^-1*/
 
-    double rho_CFT_atr = RHOC*Omega_photon*pow(atr, -4.0)*(7.0/8.0)*pow(4.0/11.0, 4.0/3.0)*DNeff;
-    double rho_ddm_atr = RHOC*Omega_ddm*pow(atr, -3.0);
+    /*double rho_CFT_atr = RHOC*Omega_photon*pow(atr, -4.0)*(7.0/8.0)*pow(4.0/11.0, 4.0/3.0)*DNeff;
+    double rho_ddm_atr = RHOC*Omega_ddm*pow(atr, -3.0);*/
+    double rho_CFT_atr = 1.73e24;
+    double rho_ddm_atr = 1.58e60;
     
     /*Original data (not log)*/
-    double *DDM = (double *)malloc(sizeof(double)*(N+1));
-    double *CFT = (double *)malloc(sizeof(double)*(N+1));
-    double *a = (double *)malloc(sizeof(double)*(N+1));
+    double jac[2][2] = {{0.0,0.0},{0.0,0.0}};
 
-    for (int i = 0; i < N; i++){
-        a[i] = log(atr) + i*h;
-        a[i] = exp(a[i]);
+    double x = log(ainit);
+    double rho_prev2[2] = {log(rho_ddm_atr) - 3*h, log(rho_CFT_atr) - 4*h};
+    double rho_prev[2];
+    double rho_next[2];
+
+    FILE *output, *params;
+    output = fopen("output.dat", "w");
+
+    memcpy(rho_prev, rho_prev2, sizeof(double)*2);
+    for (int iter = 0; iter < MAX_ITER; iter++){
+        double df[2];
+        double F[2];
+        double delta[2] = {0.0, 0.0};
+        deriv(x+h, rho_prev, df, Gamma0);
+        jacobian(x+h, rho_prev, Gamma0, jac, 1, h);
+        for (int i = 0; i < 2; i++){
+            F[i] = rho_prev[i] - rho_prev2[i] - h*df[i];
+            for (int j = 0; j < 2; j++){
+                delta[i] += -F[j]*jac[i][j];
+            }
+            rho_prev[i] += delta[i];
+        }
     }
 
-    Boltzmann(log(atr), rho_ddm_atr, rho_CFT_atr, h, N, Gamma0, DDM, CFT, "output_backward.dat");
-    Boltzmann(log(ainit), DDM[N], 1e-30, -1*h, N, Gamma0, DDM, CFT, "output_forward.dat");
-    FILE *params;
+    for (int i = 0; i < N; i++){
+        x += h;
+        bdf2(x, h, Gamma0, rho_prev, rho_prev2, rho_next, jac);
+        fprintf(output, "%e\t%e\t%e\t%e\t%e\n", x, rho_next[0], rho_next[1], H(x, exp(rho_next[0]), exp(rho_next[1])), Gamma0*exp((Gammad + 1)*x));
+
+        memcpy(rho_prev2, rho_prev, sizeof(double) * 2);
+        memcpy(rho_prev, rho_next, sizeof(double) * 2);
+    }
+
     params = fopen("params.dat", "w");
     fprintf(params, "%s\t%e\n", "aeq", AEQ);
     fprintf(params, "%s\t%e\n", "Gammad", Gammad);
     fprintf(params, "%s\t%e\n", "Gamma0", Gamma0);
     fprintf(params, "%s\t%e\n", "DNeff", DNeff);
     fprintf(params, "%s\t%e\n", "atr", atr);
-    fclose(params);
-
-    free(DDM); free(CFT); free(a);
+    fprintf(params, "%s\t%e\n", "ainit", ainit);
+    fclose(params); fclose(output);
 
     return 0;
 }
--- plotter.py
+++ plotter.py
@@ -5,11 +5,11 @@ import subprocess
 subprocess.run(["gcc", "-o", "a.out", "boltzmann.c"])
 subprocess.run(["./a.out"])
 
-dat = open("output_backward.dat", "r")
-dat_fo = open("output_forward.dat", "r")
+dat = open("output.dat", "r")
+#dat_fo = open("output_forward.dat", "r")
 paramdat = open("params.dat", "r")
 dat.readline()
-dat_fo.readline()
+#dat_fo.readline()
 
 
 params = {}
@@ -45,62 +45,30 @@ rhoCFT = np.exp(np.array(rhoCFT))
 Hs = np.array(Hs)
 Gammas = np.array(Gammas)
 
-fig, axes = plt.subplots(2,2, figsize = (16,8), constrained_layout = True)
-axes[0][0].plot(a, rhoddm, 'r-', label = r"$\rho_{\mathrm{ddm}}$")
-axes[0][0].plot(a, rhoCFT, 'b-', label = r"$\rho_{\mathrm{CFT}}$")
-axes[1][0].plot(a, Gammas/Hs, 'k-', label=r"$\Gamma(a)/H(a)$")
-axes[0][0].set_xscale('log')
-axes[0][0].set_yscale('log')
-axes[1][0].set_xscale('log')
-axes[1][0].set_yscale('log')
-axes[0][0].set_title(r"$\Gamma_d = {}$".format(params["Gammad"]))
-axes[1][0].set_title(r"$\Gamma/H$")
-axes[0][0].legend(loc=0)
-axes[0][0].set_xlabel(r"$a$")
-axes[1][0].set_xlabel(r"$a$")
-axes[0][0].set_ylabel(r"$\rho$")
-axes[1][0].set_ylabel(r"$\Gamma/H$")
+print("rhoddm = {}, rhoCFT = {}".format(rhoddm[-1], rhoCFT[-1]))
 
-a = []
-rhoddm = []
-rhoCFT = []
-Hs = []
-Gammas = []
+fig, axes = plt.subplots(2,1, figsize = (16,8), constrained_layout = True)
+axes[0].plot(a, rhoddm, 'r-', label = r"$\rho_{\mathrm{ddm}}$")
+axes[0].plot(a, rhoCFT, 'b-', label = r"$\rho_{\mathrm{CFT}}$")
+axes[1].plot(a, Gammas/Hs, 'k-', label=r"$\Gamma(a)/H(a)$")
+axes[0].set_xscale('log')
+axes[0].set_yscale('log')
+axes[1].set_xscale('log')
+axes[1].set_yscale('log')
+axes[0].set_title(r"$\Gamma_d = {}, forward$".format(params["Gammad"]))
+axes[1].set_title(r"$\Gamma/H$")
+axes[0].legend(loc=0)
+axes[0].set_xlabel(r"$a$")
+axes[1].set_xlabel(r"$a$")
+axes[0].set_ylabel(r"$\rho$ (eV$^4$)")
+axes[1].set_ylabel(r"$\Gamma/H$")
 
-
-while True:
-    l = dat_fo.readline()
-    if not l:
-        break
-    loga, logrho_ddm, logrho_CFT, H, Gamma = list(map(float, l.strip().split()))
-    a.append(loga)
-    rhoddm.append(logrho_ddm)
-    rhoCFT.append(logrho_CFT)
-    Hs.append(H)
-    Gammas.append(Gamma)
-    
-a = np.exp(np.array(a))
-rhoddm = np.exp(np.array(rhoddm))
-rhoCFT = np.exp(np.array(rhoCFT))
-Hs = np.array(Hs)
-Gammas = np.array(Gammas)
-
-axes[0][1].plot(a, rhoddm, 'r-', label = r"$\rho_{\mathrm{ddm}}$")
-axes[0][1].plot(a, rhoCFT, 'b-', label = r"$\rho_{\mathrm{CFT}}$")
-axes[1][1].plot(a, Gammas/Hs, 'k-', label=r"$\Gamma(a)/H(a)$")
-axes[0][1].set_xscale('log')
-axes[0][1].set_yscale('log')
-axes[1][1].set_xscale('log')
-axes[1][1].set_yscale('log')
-axes[0][1].set_title(r"$\Gamma_d = {}$".format(params["Gammad"]))
-axes[1][1].set_title(r"$\Gamma/H$")
-axes[0][1].legend(loc=0)
-axes[0][1].set_xlabel(r"$a$")
-axes[1][1].set_xlabel(r"$a$")
-axes[0][1].set_ylabel(r"$\rho$")
-axes[1][1].set_ylabel(r"$\Gamma/H$")
+for i in range(2):
+    axes[i].vlines(params['aeq'], 0, 1e100, colors = "black", linestyles = 'dashed')
+    axes[i].vlines(params['atr']*0.99, 0, 1e100, colors = "green", linestyles = 'dashed')
+    axes[i].set_xlim([params["ainit"], params["atr"]])
 
 
+plt.savefig("Forward_d={}.png".format(params["Gammad"]))
 plt.show()
 dat.close()
-dat_fo.close()
